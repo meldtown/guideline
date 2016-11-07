e192f1e42adef45afb74944ceaaec406a39e2614
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-07 17:17:18 +0200

    initial

--- Components/Utils/Grid.js
+++ Components/Utils/Grid.js
@@ -5,22 +5,82 @@ export default () => <DemoWrapper title="Grids">
 
 	<h2 className="fd-text-center">Two cols</h2>
 
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-left">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted fd-f1 temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-left-middle">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted fd-f1 temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-left-middle">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted fd-f1 temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-center">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-center-middle">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-around">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted temp-padding">right</li>
+		</ul>
+	</div>
+
+	<div className="fd-bg-green temp-padding">
+		<ul className="fd-f-around-middle">
+			<li className="fd-bg-light temp-padding">left<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-muted temp-padding">right</li>
+		</ul>
+	</div>
+
+	{/*
+
+.fd-f-around {display:flex;justify-content:space-around;align-items:stretch;}
+.fd-f-around-middle {display:flex;justify-content:space-around;align-items:center;}
+
+.fd-f-between {display:flex;justify-content:space-between;align-items:stretch;}
+.fd-f-between-middle {display:flex;justify-content:space-between;align-items:center;}
+	*/}
+
+
+
 	<div className="fd-bg-green fd-text-white temp-padding">
-		<ul className="fd-cols fm-rows">
-			<li className="fd-bg-light fd-cell-wide temp-padding">left</li>
+		<ul className="fd-f fm-c">
+			<li className="fd-bg-light fd-f1 temp-padding">left</li>
 			<li className="fd-bg-muted temp-padding">right</li>
 		</ul>
 	</div>
 	<div className="fd-bg-green fd-text-white temp-padding">
-		<ul className="fd-cols fm-rows">
+		<ul className="fd-f fm-c">
 			<li className="fd-bg-muted temp-padding">left</li>
-			<li className="fd-bg-light fd-cell-wide temp-padding">right</li>
+			<li className="fd-bg-light fd-f1 temp-padding">right</li>
 		</ul>
 	</div>
 	<div className="fd-bg-green fd-text-white temp-padding">
-		<ul className="fd-cols ft-rows">
-			<li className="fd-bg-muted fd-cell-wide temp-padding">left</li>
-			<li className="fd-bg-light fd-cell-wide temp-padding">right</li>
+		<ul className="fd-f ft-c">
+			<li className="fd-bg-muted fd-f1 temp-padding">left</li>
+			<li className="fd-bg-light fd-f1 temp-padding">right</li>
 		</ul>
 	</div>
 
@@ -28,19 +88,29 @@ export default () => <DemoWrapper title="Grids">
 
 	<h3 className="fd-text-center">Default top</h3>
 	<div className="fd-bg-pink fd-text-white temp-padding">
-		<ul className="fd-cols fm-rows">
-			<li className="fd-bg-light fd-cell-wide temp-padding">left</li>
-			<li className="fd-bg-muted fd-cell-wide temp-padding">center<br/>very<br/>tall<br/>cell</li>
-			<li className="fd-bg-light fd-cell-wide temp-padding">right</li>
+		<ul className="fd-f fm-c">
+			<li className="fd-bg-light fd-f1 temp-padding">left</li>
+			<li className="fd-bg-muted fd-f1 temp-padding">center<br/>very<br/>tall<br/>cell</li>
+			<li className="fd-bg-light fd-f1 temp-padding">right</li>
+		</ul>
+	</div>
+
+
+	<h3 className="fd-text-center">Middle</h3>
+	<div className="fd-bg-pink fd-text-white temp-padding">
+		<ul className="fd-f-middle fm-c-stretch">
+			<li className="fd-bg-light fd-f3 temp-padding">left</li>
+			<li className="fd-bg-muted fd-f3 temp-padding">center<br/>tall<br/>cell</li>
+			<li className="fd-bg-light fd-f3 temp-padding">right</li>
 		</ul>
 	</div>
 
 	<h3 className="fd-text-center">Middle</h3>
 	<div className="fd-bg-pink fd-text-white temp-padding">
-		<ul className="fd-cols fm-rows fd-va-middle fm-va-stretch">
-			<li className="fd-bg-light fd-cell-wide temp-padding">left</li>
-			<li className="fd-bg-muted fd-cell-wide temp-padding">center<br/>tall<br/>cell</li>
-			<li className="fd-bg-light fd-cell-wide temp-padding">right</li>
+		<ul className="fd-f-middle-middle fm-stretch-start">
+			<li className="fd-bg-light fd-f3 temp-padding">left</li>
+			<li className="fd-bg-muted fd-f3 temp-padding">center<br/>tall<br/>cell</li>
+			<li className="fd-bg-light fd-f3 temp-padding">right</li>
 		</ul>
 	</div>
 
