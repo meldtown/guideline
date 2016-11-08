1af715daebaf5b1e3e906e841bf1adffee61fa6b
RABOTA\VitaliyM <meldtown@gmail.com>
2016-11-08 16:23:57 +0200

    Updates for typo

--- Components/Atoms/Typo.js
+++ Components/Atoms/Typo.js
@@ -2,43 +2,60 @@ import React from 'react'
 import DemoWrapper from '../DemoWrapper'
 
 export default () => <DemoWrapper title="Typo">
-	<h2 className="fd-text-center">Size</h2>
+	<h2 className="fd-text-h1 fd-text-center">Size</h2>
 
 	<div className="fd-f-left fm-c-stretch">
 		<span className="fd-text-micro fd-bg-yellow fd-f1 fd-f-center-middle">Micro</span>
 		<span className="fd-text-default fd-bg-blue fd-f1 fd-f-center-middle">Default</span>
-		<span className="fd-text-large fd-bg-pink fd-f1 fd-f-center-middle">Large</span>
-		<span className="fd-text-huge fd-bg-green fd-f1 fd-f-center-middle">Huge</span>
-		<span className="fd-text-hero fd-bg-blue fd-f1 fd-f-center-middle">Hero</span>
-		<span className="fd-text-big fd-bg-yellow fd-f1 fd-f-center-middle">Big</span>
-		<span className="fd-text-loud fd-bg-lilak fd-f1 fd-f-center-middle">Loud</span>
+		<span className="fd-text-h4 fd-bg-pink fd-f1 fd-f-center-middle">H4</span>
+		<span className="fd-text-h2 fd-bg-green fd-f1 fd-f-center-middle">H2</span>
+		<span className="fd-text-h1 fd-bg-blue fd-f1 fd-f-center-middle">H1</span>
+		<span className="fd-text-h3 fd-bg-yellow fd-f1 fd-f-center-middle">H3</span>
+		<span className="fd-text-h5 fd-bg-lilak fd-f1 fd-f-center-middle">H5</span>
 		<span className="fd-text-small fd-bg-green fd-f1 fd-f-center-middle">Small</span>
 	</div>
 
-	<div className="fd-text-hero temp-padding"><b>Hero</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
-	<div className="fd-text-huge temp-padding"><b>Huge</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
-	<div className="fd-text-big temp-padding"><b>Big</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
-	<div className="fd-text-large temp-padding"><b>Large</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
-	<div className="fd-text-loud temp-padding"><b>Loud</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
+	<h1 className="fd-text-h1 fd-text-center">Multiline text</h1>
+
+	<div className="fd-text-h1 temp-padding"><b>h1</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
+	<div className="fd-text-h2 temp-padding"><b>h2</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
+	<div className="fd-text-h3 temp-padding"><b>h3</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
+	<div className="fd-text-h4 temp-padding"><b>h4</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
+	<div className="fd-text-h5 temp-padding"><b>h5</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
 	<div className="fd-text-default temp-padding"><b>Default</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
 	<div className="fd-text-small temp-padding"><b>Small</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
 	<div className="fd-text-micro temp-padding"><b>Micro</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos enim ex ratione similique voluptatem voluptatum. Aperiam culpa explicabo hic magni nam numquam optio placeat quod rerum unde! Repellendus, unde?</div>
 
 
-	<h1 className="h1 fd-text-center ft-text-right fm-text-left">Alignment</h1>
+	<h1 className="fd-text-h1 fd-text-center">Text colors</h1>
+
+	<div  className="fd-text-light fd-bg-green  fd-text-h3 temp-padding"><b className="fd-text-black">light </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-dark fd-bg-green fd-text-h3 temp-padding"><b className="fd-text-black">dark </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-black fd-bg-green  fd-text-h3 temp-padding"><b className="fd-text-black">black </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-white fd-bg-dark  fd-text-h3 temp-padding"><b className="fd-text-black">white </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-main fd-bg-green  fd-text-h3 temp-padding"><b className="fd-text-black">main </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-primary fd-bg-green  fd-text-h3 temp-padding"><b className="fd-text-black">primary </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-secondary fd-bg-green  fd-text-h3 temp-padding"><b className="fd-text-black">secondary </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-blue fd-bg-dark fd-text-h3 temp-padding"><b className="fd-text-black">blue </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-green fd-bg-dark  fd-text-h3 temp-padding"><b className="fd-text-black">green </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-yellow fd-bg-dark  fd-text-h3 temp-padding"><b className="fd-text-black">yellow </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-pink fd-bg-dark fd-text-h3 temp-padding"><b className="fd-text-black">pink </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+	<div  className="fd-text-lilak fd-bg-dark  fd-text-h3 temp-padding"><b className="fd-text-black">lilak </b>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab amet at culpa cum cupiditate dolores est facilis laborum neque nesciunt nulla numquam perspiciatis quae quisquam repudiandae ullam, vero voluptate!</div>
+
+	<h1 className=" fd-text-h1 fd-text-center ft-text-right fm-text-left">Alignment</h1>
 
 	<ul className="sg_row">
-		<li className="wide"><p className="text-left">Left</p></li>
-		<li className="wide"><p className="text-center">Center</p></li>
-		<li className="wide"><p className="text-right">Right</p></li>
+		<li className="temp-padding wide"><p className="text-left">Left</p></li>
+		<li className="temp-padding wide"><p className="text-center">Center</p></li>
+		<li className="temp-padding wide"><p className="text-right">Right</p></li>
 	</ul>
 
 
-	<h1>h1 by default is same as usual text</h1>
-	<p className="h1">h1 may be added to any element</p>
-	<p>even <span className="h1">span</span></p>
+	<h1 className="temp-padding">h1 by default is same as usual text</h1>
+	<p className="temp-padding fd-text-h1">h1 may be added to any element</p>
+	<p className="temp-padding">even <span className="fd-text-h1">span</span></p>
 
-	<ul className="sg_row">
+	<ul className="temp-padding sg_row">
 		<li>
 			<h1 className="h1">Heading 1 One line</h1>
 		</li>
