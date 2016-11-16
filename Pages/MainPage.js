a656216cb21e7f6c8085b2b363ad6d1d6a12367c
Vlad Feninets <v.feninets@gmail.com>
2016-11-16 16:42:28 +0200

    fix link

--- Pages/MainPage.js
+++ Pages/MainPage.js
@@ -15,6 +15,5 @@ export default () => <div style={{ marginTop: 100 }} >
 	<Link style={linkStyle} to="/colors">Colors</Link>
 	<Link style={linkStyle} to="/typo">Typo</Link>
 	<Link style={linkStyle} to="/grid">Grid</Link>
-	<Link style={linkStyle} to="/input-icon">Input Icon</Link>
 	<Link style={linkStyle} to="/controls">Controls</Link>
 </div>
