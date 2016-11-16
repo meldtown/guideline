8e47a76baf619a46c12ff9f8069d686f3559071d
RABOTA\VitaliyM <meldtown@gmail.com>
2016-11-16 12:58:12 +0200

    Primary and control buttons havebeen added

--- Components/Atoms/Buttons.js
+++ Components/Atoms/Buttons.js
@@ -4,28 +4,31 @@ import DemoWrapper from '../DemoWrapper'
 export default () => <DemoWrapper title="Buttons">
 	<div className="temp-padding">
 
-		<h3 className="fd-h3 fd-text-center">Inline level buttons</h3>
+		<h3 className="fd-h3 fd-text-center">Primary button</h3>
 
-		<button className="btn-default fd-h1">btn-default</button>
-		<button className="btn-border fd-h1">btn-border</button>
-		<button className="btn-filled fd-h1">btn-filled</button>
-		<button className="btn-default-hovered-border fd-h1">btn-default-hovered-border</button>
-		<button className="btn-default-hovered-filled fd-h1">btn-default-hovered-filled</button>
-		<button className="btn-border-hovered-filled fd-h1">btn-border-hovered-filled</button>
+		<button className="btn-primary fd-h5">найти сейчас</button>
 
-		<h3 className="fd-h3 fd-text-center">Block level buttons</h3>
+		<h3 className="fd-h3 fd-text-center">Buttons with controls</h3>
 
-		<button className="btn-default-block fd-h1">btn-default-block</button>
-		<button className="btn-border-block fd-h1">btn-border-block</button>
-		<button className="btn-filled-block fd-h1">btn-filled-block</button>
-		<button className="btn-default-hovered-border-block fd-h1">btn-default-hovered-border-block</button>
-		<button className="btn-default-hovered-filled-block fd-h1">btn-default-hovered-filled-block</button>
-		<button className="btn-border-hovered-filled-block fd-h1">btn-border-hovered-filled-block</button>
+		<button className="btn-control-coral fd-h5">
+			<span>Создать резюме</span>
+			<i className="fa fa-arrow-right"></i>
+		</button>
+		<br/>
+
+		<button className="btn-control-royal-blue fd-h5">
+			<span>Перейти</span>
+			<i className="fa fa-arrow-right"></i>
+		</button>
+		<br/>
+
+		<button className="btn-control-dark-green fd-h5">
+			<span>Сохранить</span>
+			<i className="fa fa-check"></i>
+		</button>
+		<br/>
 
 		<h3 className="fd-h3 fd-text-center">Link buttons</h3>
-
-		<a href="#" className="btn-border-hovered-filled fd-h1">btn-border-hovered-filled</a>
-		<a href="#" className="btn-border-hovered-filled-block fd-h1">btn-border-hovered-filled-block</a>
 	</div>
 
 </DemoWrapper>
