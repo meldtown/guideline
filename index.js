cb9a8317a6b547bbcc622facf8793f95f078f971
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-17 14:52:22 +0200

    fix for base url initial

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import 'isomorphic-fetch'
 import React from 'react'
 import {render} from 'react-dom'
-import {Router, browserHistory} from 'react-router'
+import {Router, useRouterHistory} from 'react-router'
 import Routes from './routes.js'
+import {createHistory} from 'history'
 
 const root = document.createElement('div')
 document.body.appendChild(root)
 
+const browserHistory = useRouterHistory(createHistory)({
+	basename: window.location.hostname === 'localhost' ? '/' : window.location.pathname
+})
+
 render(
-	<Router history={browserHistory} routes={Routes} />,
+	<Router history={browserHistory} routes={Routes}/>,
 	root
 )
