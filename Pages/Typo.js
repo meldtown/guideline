5660cde3943a5f1af0f0787f407f1759d8dd8988
RABOTA\VitaliyM <meldtown@gmail.com>
2016-11-17 14:03:25 +0200

    Multiline text have been added

--- Pages/Typo.js
+++ Pages/Typo.js
@@ -20,14 +20,77 @@ export default () => <DemoWrapper title="Typo">
 		<p style={{margin: '10px 0'}} className="fd-block-header">Blocks-Header</p>
 		<p style={{margin: '10px 0'}} className="fd-vacancy-header">Vacancy-Header</p>
 		<p style={{margin: '10px 0'}} className="fd-vacancy-title">Vacancy-Title</p>
-		<p style={{margin: '10px 0'}}><span className="fd-form-header">Forms-Header</span> / <span className="fd-subheader">Subheader</span></p>
+		<p style={{margin: '10px 0'}}><span className="fd-form-header">Forms-Header</span> / <span
+			className="fd-subheader">Subheader</span></p>
 		<p style={{margin: '10px 0'}} className="fd-subtitle">Subtitle</p>
-		<p style={{margin: '10px 0'}}><span className="fd-regular">Default-text</span> / <span className="fd-light">Default-text</span> / <span className="fd-medium">Default-text</span> / <span className="fd-semibold">Default-text</span></p>
-		<p style={{margin: '10px 0'}} className="fd-middle"><span className="fd-light">Middle-text</span> / <span className="fd-regular">Middle-text</span></p>
+		<p style={{margin: '10px 0'}}><span className="fd-regular">Default-text</span> / <span className="fd-light">Default-text</span>
+			/ <span className="fd-medium">Default-text</span> / <span className="fd-semibold">Default-text</span></p>
+		<p style={{margin: '10px 0'}} className="fd-middle"><span className="fd-light">Middle-text</span> / <span
+			className="fd-regular">Middle-text</span></p>
 		<p style={{margin: '10px 0'}} className="fd-small">Small text</p>
 		<p style={{margin: '10px 0'}} className="fd-micro">Smallest text</p>
 	</div>
 
+	<h1 className="fd-text-center fd-subheader">Multiline text</h1>
 
-
+	<div className="temp-padding">
+		<p className="fd-header">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,amet, consectetur adipisicing elit.
+		</p>
+		<p className="fd-section-header">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur dolore excepturi maxime optio pariatur possimus quas similique
+		</p>
+		<p className="fd-block-header">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut, dicta enim inventore ipsam minima officiis quia sunt suscipit! Aut.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam atque beatae
+		</p>
+		<p className="fd-vacancy-header">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur debitis dolore facere impedit iste nesciunt officia quis ratione sequi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut commodi consequatur
+			dicta
+		</p>
+		<p className="fd-vacancy-title">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis consequatur doloremque esse, lau Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias blanditiis consequatur doloremque esse, lau
+		</p>
+		<p className="fd-subheader">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere laudantium quod tenetur veritatis vero? Earum facere fugiat iste quibusdam repudiandae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, at
+			cupiditate earum enim est magnam magni veritatis. Amet assumenda aut
+		</p>
+		<p className="fd-form-header">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, culpa fuga inventore iste minima molestiae officiis optio recusandae sint velit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus
+			cupiditate ea esse eveniet in incidunt inventore iure maxime
+		</p>
+		<p className="fd-subtitle">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt distinctio earum error id, laborum modi nam sequi ut vel voluptates.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab eos fugiat
+			iusto nulla. Accusamus debitis doloremque, iure minima modi repellat! Asperiores eos excepturi in magni
+			mollitia
+			ut,
+			veritatis. Iusto, quis.
+		</p>
+		<p className="fd-default">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque debitis, eius eos eveniet ipsum pariatur perspiciatis repellendus sed velit voluptatum.
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium aperiam commodi dolor dolore dolorem
+			eum
+			iusto magni minima non quae quam quisquam quos repellendus similique soluta, temporibus? Ipsum, tenetur!
+		</p>
+		<p className="fd-middle">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi dolore est libero modi odit possimus quam quibusdam rem repellat.
+			Lorem
+			ipsum dolor sit amet, consectetur adipisicing elit. Alias amet culpa cumque deleniti dolor eligendi, error
+			explicabo
+			libero nisi nobis nostrum numquam odit quibusdam quidem, quos similique vero voluptate. Sit.
+		</p>
+		<p className="fd-small">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dignissimos, earum id incidunt magni molestias praesentium quaerat quas unde veritatis?Lorem ipsum dolor
+			sit amet, consectetur adipisicing elit. A beatae delectus modi nisi, placeat quae quod recusandae
+			voluptatibus!
+			Commodi corporis deserunt eligendi impedit magni, odio repudiandae voluptatibus. Deserunt, exercitationem,
+			minus?
+		</p>
+		<p className="fd-micro">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam dignissimos, earum id incidunt magni molestias praesentium quaerat quas unde veritatis?Lorem ipsum dolor
+			sit amet, consectetur adipisicing elit. A beatae delectus modi nisi, placeat quae quod recusandae
+			voluptatibus!
+			Commodi corporis deserunt eligendi impedit magni, odio repudiandae voluptatibus. Deserunt, exercitationem,
+			minus?
+		</p>
+	</div>
 </DemoWrapper>
