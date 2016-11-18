97a9e69e84602690a2341517f237e7c3994c046d
Vlad Feninets <v.feninets@gmail.com>
2016-11-18 14:32:00 +0200

    changed MainPage

--- Pages/MainPage.js
+++ Pages/MainPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import '../mainpage_styles.scss'
 
-const linkStyle = {
+
+/*const linkStyle = {
 	display: 'block',
 	fontSize: 30,
 	textAlign: 'center',
@@ -18,3 +20,53 @@ export default () => <div style={{ marginTop: 100 }} >
 	<Link style={linkStyle} to="/controls">Controls</Link>
 	<Link style={linkStyle} to="/papers">Papers</Link>
 </div>
+*/
+
+
+export default () => <div>
+		<section className="temp-strips">
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/buttons">Buttons</Link>
+		      </h1>		      
+		    </div>
+		  </article>
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/colors">Colors</Link>
+		      </h1>
+		    </div>
+		  </article>
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/typo">Typo</Link>
+		      </h1>
+		    </div>
+		  </article>
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/controls">Controls</Link>
+		      </h1>
+		    </div>
+		  </article>
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/grid">Grid</Link>
+		      </h1>
+		    </div>
+		  </article>
+		  <article className="temp-strips__strip">
+		    <div className="strip__content">
+		      <h1 className="strip__title">
+		      	<Link to="/papers">Papers</Link>
+		      </h1>
+		    </div>
+		  </article>
+		  <i className="fa fa-close strip__close"></i>
+		</section>
+	</div>	
