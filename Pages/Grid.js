cc8995cafe4a323b5f736bf1f5429f63111e9d87
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-18 13:12:33 +0200

    greedy grids

--- Pages/Grid.js
+++ Pages/Grid.js
@@ -9,7 +9,11 @@ export default () => <DemoWrapper title="Grids">
 
 		<h2 className="fd-section-header fd-text-center">Horizontal alignment</h2>
 
-		<p>All grids starts with <span className="f-text-gray">fd-[horizontal-alignment]<span className="f-text-light-gray">-[vertical-aligment]</span></span> class by default vertical alignment is kinda "stretch" and is optional, so your options are: <strong className="f-text-lilac">fd-f-left</strong>, <strong className="f-text-lilac">fd-f-center</strong>, <strong className="f-text-lilac">fd-f-right</strong></p>
+		<p>All grids starts with <span className="f-text-gray">fd-[horizontal-alignment]<span
+			className="f-text-light-gray">-[vertical-aligment]</span></span> class by default vertical alignment is
+			kinda "stretch" and is optional, so your options are: <strong className="f-text-lilac">fd-f-left</strong>,
+			<strong className="f-text-lilac">fd-f-center</strong>, <strong className="f-text-lilac">fd-f-right</strong>
+		</p>
 
 		<h3 className="fd-subheader fd-text-center">Left</h3>
 
@@ -34,7 +38,8 @@ export default () => <DemoWrapper title="Grids">
 
 		<h2 className="fd-section-header fd-text-center">Vertical alignment</h2>
 
-		<p>The only available option for vertical alignment is <strong className="f-text-lilac">middle</strong> so you may have classes like <span>fd-f-center-middle</span></p>
+		<p>The only available option for vertical alignment is <strong className="f-text-lilac">middle</strong> so you
+			may have classes like <span>fd-f-center-middle</span></p>
 
 		<p className="fd-f-center-middle fm-c-stretch">
 			<span className="f-bg-light-gray temp-padding">left</span>
@@ -44,7 +49,9 @@ export default () => <DemoWrapper title="Grids">
 
 		<h2 className="fd-section-header fd-text-center">12 cols is dead</h2>
 
-		<p>Flex grid does not need 12 cols all we need is already here in flex box, so we have added <strong className="f-text-lilac">fd-f1</strong>, <strong className="f-text-lilac">fd-f2</strong>, <strong className="f-text-lilac">fd-f3</strong> which may be applied to cells to make them proportional wide</p>
+		<p>Flex grid does not need 12 cols all we need is already here in flex box, so we have added <strong
+			className="f-text-lilac">fd-f1</strong>, <strong className="f-text-lilac">fd-f2</strong>, <strong
+			className="f-text-lilac">fd-f3</strong> which may be applied to cells to make them proportional wide</p>
 
 		<p className="fd-f-left fm-c-stretch">
 			<span className="f-bg-light-gray temp-padding">left</span>
@@ -59,7 +66,8 @@ export default () => <DemoWrapper title="Grids">
 
 		<h2 className="fd-section-header fd-text-center">Misc</h2>
 
-		<p>There is two additional horizontal alignments available <strong className="f-text-lilac">fd-f-around</strong> and <strong className="f-text-lilac">fd-f-between</strong></p>
+		<p>There is two additional horizontal alignments available <strong className="f-text-lilac">fd-f-around</strong>
+			and <strong className="f-text-lilac">fd-f-between</strong></p>
 
 		<p className="fd-f-around fm-c-stretch">
 			<span className="f-bg-light-gray temp-padding">left</span>
@@ -72,6 +80,37 @@ export default () => <DemoWrapper title="Grids">
 			<span className="f-bg-light-light-gray temp-padding">center</span>
 			<span className="f-bg-light-gray temp-padding">right</span>
 		</p>
+
+		<h2 className="fd-section-header fd-text-center">Greedy grids</h2>
+		<p>By combining grids inside each other you may produce any kind of component</p>
+
+		<p className="fd-f-center">
+			<span className="fd-f-left fm-c-center">
+				<span className="fd-f-left fm-c-center">
+					<span className="f-bg-light-gray temp-padding"><i className="fi-ok"/></span>
+					<span className="f-bg-light-light-gray temp-padding">hello world</span>
+				</span>
+				<span className="fd-f-left fm-c-center">
+					<span className="f-bg-light-gray temp-padding"><i className="fi-like"/></span>
+					<span className="f-bg-light-light-gray temp-padding">lorem ipsum</span>
+				</span>
+			</span>
+		</p>
+
+		{[1, 2, 3].map(i => <div key={i} className="fd-f-left-middle ">
+			<span className="temp-padding"><i className="fi-contact f-text-dark-green"/></span>
+			<span className="fd-f1 fd-f-left-middle fm-c-stretch">
+			<span>
+				<strong>User name {i}</strong>
+				<br />
+				<span className="f-text-light-gray">Hi, how are...</span>
+			</span>
+			<span className="f-text-light-gray fd-small">2 hours ago</span>
+		</span>
+			<span className="temp-padding fm-hidden"><i className="fi-arrows-right-big f-text-coral"/></span>
+		</div>)}
+
+		<h2 className="fd-section-header fd-text-center">Media queries</h2>
 	</div>
 
 
