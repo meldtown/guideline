06a7e2c3fa83ed0933d471d52840a75ff7893e1e
RABOTA\VitaliyM <meldtown@gmail.com>
2016-11-18 15:41:08 +0200

    Prefixes for buttons classes have been added

--- Pages/Buttons.js
+++ Pages/Buttons.js
@@ -6,88 +6,88 @@ export default () => <DemoWrapper title="Buttons">
 
 		<h3 className="fd-block-header fd-text-center">Primary button</h3>
 
-		<a href="#" style={{color: '#2956ac'}} className="link fd-default">Default link</a><br/><br/>
-		<a href="#" className="link fd-default">Visited link</a>
+		<a href="#" style={{color: '#2956ac'}} className="f-link fd-default">Default link</a><br/><br/>
+		<a href="#" className="f-link fd-default">Visited link</a>
 
 		<h3 className="fd-block-header fd-text-center">Primary button</h3>
 
-		<button className="btn-primary fd-vacancy-title">найти сейчас</button>
+		<button className="f-btn-primary fd-vacancy-title">найти сейчас</button>
 
 		<h3 className="fd-block-header fd-text-center">Buttons with controls</h3>
 
-		<button className="btn-control-coral fd-vacancy-title">
+		<button className="f-btn-control-coral fd-vacancy-title">
 			<span>Создать резюме</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 
-		<button className="btn-control-coral disabled fd-vacancy-title">
+		<button className="f-btn-control-coral f-disabled fd-vacancy-title">
 			<span>Создать резюме</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 
 		<br/>
 
-		<button className="btn-control-royal-blue fd-vacancy-title">
+		<button className="f-btn-control-royal-blue fd-vacancy-title">
 			<span>Перейти</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 
-		<button className="btn-control-royal-blue fd-vacancy-title disabled">
+		<button className="f-btn-control-royal-blue fd-vacancy-title f-disabled">
 			<span>Перейти</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 		<br/>
 
-		<button className="btn-control-dark-green fd-vacancy-title">
+		<button className="f-btn-control-dark-green fd-vacancy-title">
 			<span>Сохранить</span>
 			<i className="fi-ok" />
 		</button>
 
-		<button className="btn-control-dark-green disabled fd-vacancy-title">
+		<button className="f-btn-control-dark-green f-disabled fd-vacancy-title">
 			<span>Сохранить</span>
 			<i className="fi-ok" />
 		</button>
 
 		<h3 className="fd-block-header fd-text-center">Link-like buttons with controls</h3>
 
-		<button className="btn-control-coral-link fd-vacancy-title">
+		<button className="f-btn-control-coral-link fd-vacancy-title">
 			<span>Перейти</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 
-		<button className="btn-control-coral-link disabled fd-vacancy-title">
-			<span>Перейти</span>
-			<i className="fi-arrows-right-big" />
-		</button>
-		<br/>
-
-		<button className="btn-control-royal-blue-link fd-vacancy-title">
-			<span>Перейти</span>
-			<i className="fi-arrows-right-big" />
-		</button>
-
-		<button className="btn-control-royal-blue-link disabled fd-vacancy-title">
+		<button className="f-btn-control-coral-link f-disabled fd-vacancy-title">
 			<span>Перейти</span>
 			<i className="fi-arrows-right-big" />
 		</button>
 		<br/>
 
-		<button className="btn-control-dark-green-link fd-vacancy-title">
+		<button className="f-btn-control-royal-blue-link fd-vacancy-title">
+			<span>Перейти</span>
+			<i className="fi-arrows-right-big" />
+		</button>
+
+		<button className="f-btn-control-royal-blue-link f-disabled fd-vacancy-title">
+			<span>Перейти</span>
+			<i className="fi-arrows-right-big" />
+		</button>
+		<br/>
+
+		<button className="f-btn-control-dark-green-link fd-vacancy-title">
 			<span>Перейти</span>
 			<i className="fi-ok" />
 		</button>
 
-		<button className="btn-control-dark-green-link disabled fd-vacancy-title">
+		<button className="f-btn-control-dark-green-link f-disabled fd-vacancy-title">
 			<span>Перейти</span>
 			<i className="fi-ok" />
 		</button>
 
-		<button className="btn-control-default fd-vacancy-title">
+		<button className="f-btn-control-default fd-vacancy-title">
 			<span>Зарегистрироваться</span>
 			<i className="fi-small-arrow" />
 		</button>
 
-		<button className="btn-control-default disabled fd-vacancy-title">
+		<button className="f-btn-control-default f-disabled fd-vacancy-title">
 			<span>Зарегистрироваться</span>
 			<i className="fi-small-arrow" />
 		</button>
