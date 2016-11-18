e5c52cd7b0cb3049f9779f1a56d1710507850c30
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-18 15:24:23 +0200

    s3 preparation

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node
-MAINTAINER Alexandr Marchenko <marchenko.alexandr@gmail.com>
-
-COPY package.json /package.json
-RUN npm install -s
-RUN mkdir /code
-WORKDIR /code
-
--- deploy.js
+++ deploy.js
@@ -1,28 +1,25 @@
-var fs = require('fs');
-var mime = require('mime-types');
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3({
-    signatureVersion: 'v4',
-    region: 'eu-central-1'
-});
+const fs = require('fs')
+const mime = require('mime-types')
+const AWS = require('aws-sdk')
+
+const s3 = new AWS.S3({signatureVersion: 'v4', region: 'eu-central-1'})
 
 fs.readdir('./dist', (err, files) => {
 	files.forEach(file => {
 		fs.readFile('./dist/' + file, function (err, data) {
-			if (err) console.log(err)
-			else {
-				var params = {
-					Bucket: 'pornushka',
-					Key: 'fedor/' + file,
-					Body: data,
-					ContentType: mime.lookup(file)
-				};
+			if (err) throw err
 
-				s3.putObject(params, function (err) {
-					if (err) console.log(err)
-					else console.log(file + ' uploaded');
-				});
+			let params = {
+				Bucket: 'rua-assets',
+				Key: 'fedor/' + file,
+				Body: data,
+				ContentType: mime.lookup(file)
 			}
-		});
-	});
-});
+
+			s3.putObject(params, function (err) {
+				if (err) console.log(err)
+				else console.log(file + ' uploaded')
+			})
+		})
+	})
+})
