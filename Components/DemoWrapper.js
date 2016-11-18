85780751c470cc9f20f9837b47afd4d5a0c1903d
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-18 13:38:53 +0200

    demo wrapper fix

--- Components/DemoWrapper.js
+++ Components/DemoWrapper.js
@@ -1,32 +1,28 @@
 import React from 'react'
 
 export default ({title, children}) => <div className="demo">
-	<h1 className="fd-text-center fd-header">{title}</h1>
 	<input type="checkbox" id="overlay"/>
-	<div style={{ backgroundColor: '#ebf3fa', paddingBottom: '100px' }}>
+	<div style={{backgroundColor: '#ebf3fa', paddingBottom: '100px'}}>
 
-		<h2 className="fd-section-header temp-padding">Blocks</h2>
-
-
-		<header className="f-paper" style={{ width: '100%', marginBottom: '100px', position: 'relative' }}>
-			<p style={{ padding: '50px', textAlign: 'center', fontSize: '23px', fontWeight: '700' }}>
-				MENU BLOCK
+		<header className="f-paper" style={{width: '100%', marginBottom: '100px', position: 'relative'}}>
+			<div style={{padding: '50px', textAlign: 'center', fontSize: '23px', fontWeight: '700'}}>
+				{title}
 				<label className="menu-icon" htmlFor="overlay">
-					<i className="fi-vacancy"></i>
+					<i className="fi-vacancy"/>
 				</label>
-			</p>
+			</div>
 		</header>
 
-			<div className="f-paper f-main-wrapper" style={{ padding: '50px' }}>
-				{children}
-			</div>
+		<div className="f-paper f-main-wrapper" style={{padding: '50px'}}>
+			{children}
+		</div>
 
 		<div className="f-paper-lighten">
-			<p style={{ padding: '70px', textAlign: 'center' }}>Lorem ipsum dolor</p>
+			<p style={{padding: '70px', textAlign: 'center'}}>Lorem ipsum dolor</p>
 		</div>
 	</div>
 
-	<label className="overlay" htmlFor="overlay"></label>
+	<label className="overlay" htmlFor="overlay"/>
 
 	<aside className="sidebar">
 		<label className="close-sidebar" htmlFor="overlay">
