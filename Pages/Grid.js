f0849e2d39b84ca2f8488cb7fc9d715722e5d68c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-18 12:30:44 +0200

    grids wip

--- Pages/Grid.js
+++ Pages/Grid.js
@@ -9,20 +9,69 @@ export default () => <DemoWrapper title="Grids">
 
 		<h2 className="fd-section-header fd-text-center">Horizontal alignment</h2>
 
+		<p>All grids starts with <span className="f-text-gray">fd-[horizontal-alignment]<span className="f-text-light-gray">-[vertical-aligment]</span></span> class by default vertical alignment is kinda "stretch" and is optional, so your options are: <strong className="f-text-lilac">fd-f-left</strong>, <strong className="f-text-lilac">fd-f-center</strong>, <strong className="f-text-lilac">fd-f-right</strong></p>
+
 		<h3 className="fd-subheader fd-text-center">Left</h3>
 
-		<ul className="fd-f-left fm-c-stretch">
-			<li className="f-bg-light-gray temp-padding">left</li>
-			<li className="f-bg-light-light-gray temp-padding">right<br/>Notice that both cells has same height</li>
-		</ul>
+		<p className="fd-f-left fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">right<br/>Notice that both cells has same height</span>
+		</p>
 
-		<p className="fd-text-center">If you want you may configure cells width by adding classes like .fd-f1</p>
+		<h3 className="fd-subheader fd-text-center">Center</h3>
 
-		<ul className="fd-f-left fm-c-stretch">
-			<li className="f-bg-light-gray temp-padding">left</li>
-			<li className="f-bg-light-light-gray fd-f1 temp-padding">right<br/>Notice that both cells has same height</li>
-		</ul>
+		<p className="fd-f-center fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">right</span>
+		</p>
 
+		<h3 className="fd-subheader fd-text-center">Right</h3>
+
+		<p className="fd-f-right fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">right</span>
+		</p>
+
+		<h2 className="fd-section-header fd-text-center">Vertical alignment</h2>
+
+		<p>The only available option for vertical alignment is <strong className="f-text-lilac">middle</strong> so you may have classes like <span>fd-f-center-middle</span></p>
+
+		<p className="fd-f-center-middle fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">hello<br />world</span>
+			<span className="f-bg-light-gray temp-padding">right</span>
+		</p>
+
+		<h2 className="fd-section-header fd-text-center">12 cols is dead</h2>
+
+		<p>Flex grid does not need 12 cols all we need is already here in flex box, so we have added <strong className="f-text-lilac">fd-f1</strong>, <strong className="f-text-lilac">fd-f2</strong>, <strong className="f-text-lilac">fd-f3</strong> which may be applied to cells to make them proportional wide</p>
+
+		<p className="fd-f-left fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray fd-f1 temp-padding">If you want you may configure cells width by adding classes like <strong>fd-f1</strong></span>
+		</p>
+
+		<p className="fd-f-left fm-c-stretch">
+			<span className="f-bg-light-gray fd-f1 temp-padding">fd-f1</span>
+			<span className="f-bg-light-light-gray fd-f2 temp-padding">fd-f2</span>
+			<span className="f-bg-light-gray fd-f1 temp-padding">fd-f1</span>
+		</p>
+
+		<h2 className="fd-section-header fd-text-center">Misc</h2>
+
+		<p>There is two additional horizontal alignments available <strong className="f-text-lilac">fd-f-around</strong> and <strong className="f-text-lilac">fd-f-between</strong></p>
+
+		<p className="fd-f-around fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">center</span>
+			<span className="f-bg-light-gray temp-padding">right</span>
+		</p>
+
+		<p className="fd-f-between fm-c-stretch">
+			<span className="f-bg-light-gray temp-padding">left</span>
+			<span className="f-bg-light-light-gray temp-padding">center</span>
+			<span className="f-bg-light-gray temp-padding">right</span>
+		</p>
 	</div>
 
 
