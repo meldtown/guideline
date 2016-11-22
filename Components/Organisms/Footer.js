1fe8d4aa8d6ff0cff4d403721bef3db5c829c9a8
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2016-11-22 11:29:58 +0200

    footer wip

--- Components/Organisms/Footer.js
+++ Components/Organisms/Footer.js
@@ -1,114 +1,55 @@
 import React from 'react'
 
-export default () => <div className="f-main-wrapper">
-	<ul className="fd-f-between fd-micro">
-		<li>
-			<i className="fi-checked-company fd-header" />
-		</li>
-		<li>
-			<p>
-				<strong className="fd-small">rabota.ua</strong>
-			</p>
-			<ul className="f-reset-list">
-				<li>
-					<a href="#">rabota.ua</a>
-				</li>
-				<li>
-					<a href="#">Конфиденциальность</a>
-				</li>
-				<li>
-					<a href="#">Карта сайта</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<p>
-				<strong className="fd-small">соискателю</strong>
-			</p>
-			<ul className="f-reset-list">
-				<li>
-					<a href="#">Найти работу</a>
-				</li>
-				<li>
-					<a href="#">Разместить резюме</a>
-				</li>
-				<li>
-					<a href="#">Рассылка вакансий</a>
-				</li>
-				<li>
-					<a href="#">Как составить резюме</a>
-				</li>
-				<li>
-					<a href="#">Советы соискателю</a>
-				</li>
-				<li>
-					<a href="#">Архив вакансий</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<p>
-				<strong className="fd-small">работодателю</strong>
-			</p>
-			<ul className="f-reset-list">
-				<li>
-					<a href="#">Добавить вакансию</a>
-				</li>
-				<li>
-					<a href="#">Поиск резюме</a>
-				</li>
-				<li>
-					<a href="#">Рассылка резюме</a>
-				</li>
-				<li>
-					<a href="#">Наши услуги</a>
-				</li>
-				<li>
-					<a href="#">Советы работодателю</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<p>
-				<strong className="fd-small">популярные города</strong>
-			</p>
-			<ul className="f-reset-list">
-				<li>
-					<a href="#">Работа в Киеве</a>
-				</li>
-				<li>
-					<a href="#">Работа в Днепре (Днепропетровске)</a>
-				</li>
-				<li>
-					<a href="#">Работа в Донецке</a>
-				</li>
-				<li>
-					<a href="#">Работа в Харькове</a>
-				</li>
-				<li>
-					<a href="#">Работа в Одессе</a>
-				</li>
-				<li>
-					<a href="#">Работа во Львове</a>
-				</li>
-				<li>
-					<a href="#">Работа в Запорожье</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<div className="f-input-block fd-default">
-				<select>
-					<option>Русский</option>
-					<option>Украинский</option>
-				</select>
-				<i className="fi-drop-down-icon" />
+export default () => <footer className="f-footer">
+	<div className="f-main-wrapper">
+		<div className="fd-f-between fd-micro">
+			<i className="fi-logo-symbol"/>
+			<nav>
+				<span className="fd-semibold fd-small">rabota.ua</span>
+				<a href="#">rabota.ua</a>
+				<a href="#">Конфиденциальность</a>
+				<a href="#">Карта сайта</a>
+			</nav>
+			<nav>
+				<span className="fd-semibold fd-small">соискателю</span>
+				<a href="#">Найти работу</a>
+				<a href="#">Разместить резюме</a>
+				<a href="#">Рассылка вакансий</a>
+				<a href="#">Как составить резюме</a>
+				<a href="#">Советы соискателю</a>
+				<a href="#">Архив вакансий</a>
+			</nav>
+			<nav>
+				<span className="fd-semibold fd-small">работодателю</span>
+				<a href="#">Добавить вакансию</a>
+				<a href="#">Поиск резюме</a>
+				<a href="#">Рассылка резюме</a>
+				<a href="#">Наши услуги</a>
+				<a href="#">Советы работодателю</a>
+			</nav>
+			<nav>
+				<span className="fd-semibold fd-small">популярные города</span>
+				<a href="#">Работа в Киеве</a>
+				<a href="#">Работа в Днепре (Днепропетровске)</a>
+				<a href="#">Работа в Донецке</a>
+				<a href="#">Работа в Харькове</a>
+				<a href="#">Работа в Одессе</a>
+				<a href="#">Работа во Львове</a>
+				<a href="#">Работа в Запорожье</a>
+			</nav>
+			<div className="f-footer--feedback-col">
+				<div className="f-input-block fd-default">
+					<select>
+						<option>Русский</option>
+						<option>Украинский</option>
+					</select>
+					<i className="fi-drop-down-icon"/>
+				</div>
+				<div className="fd-small">
+					<a href="#">Написать <strong>rabota.ua</strong> <i className="fi-small-arrow"/></a>
+					Присылайте свои замечания и пожелания по работе и наполнению портала.
+				</div>
 			</div>
-			<p className="fd-small">
-				<a href="#">Написать <strong>rabota.ua</strong> <i className="fi-small-arrow" /></a>
-				<br />
-				Присылайте свои замечания и пожелания по работе и наполнению портала.
-			</p>
-		</li>
-	</ul>
-</div>
+		</div>
+	</div>
+</footer>
