4e7581070834e4f04c54590b8e44cc38ba8db960
Vlad Feninets <v.feninets@gmail.com>
2016-11-29 17:49:55 +0200

    fix transition on links

--- Components/SidebarMenu.js
+++ Components/SidebarMenu.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router'
 
 
 export default () => <div class="fd-merchant fd-p20" style={{ marginTop: '90px' }} >
-	<Link to="/" class="f-always-blue f-btn-control-royal-blue-link fd-samurai">
+	<Link to="/" class="f-always-blue f-btn-control-royal-blue-link fd-soldier">
 		<span>Homepage</span>
 		<i class="fi-arrows-right-big" />
 	</Link>
+
+	<br/><br/>
 
 	<ul class="temp-sidebar-ul f-reset-list fd-p20" style={{ paddingLeft: '25px' }}>
 		<li><Link to="/buttons" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Buttons</Link></li>
