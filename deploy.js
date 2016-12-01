ceca92df4d24b72494b509bb51f220ab7c2f1a26
Vlad Feninets <v.feninets@gmail.com>
2016-12-01 10:45:41 +0200

    merge

--- deploy.js
+++ deploy.js
@@ -1,25 +1,37 @@
 const fs = require('fs')
 const mime = require('mime-types')
+const git = require('git-rev')
 const AWS = require('aws-sdk')
 
 const s3 = new AWS.S3({signatureVersion: 'v4', region: 'eu-central-1'})
 
-fs.readdir('./dist', (err, files) => {
-	files.forEach(file => {
-		fs.readFile('./dist/' + file, function (err, data) {
-			if (err) throw err
+const branch2bucket = {
+	master: 'rua-assets-frankfurt',
+	dev: 'rua-assets-dev'
+}
 
-			let params = {
-				Bucket: 'rua-assets',
-				Key: 'fedor/' + file,
-				Body: data,
-				ContentType: mime.lookup(file)
-			}
+git.branch(branch => {
+	if (branch2bucket[branch]) {
+		fs.readdir('./dist', (err, files) => {
+			files.forEach(file => {
+				fs.readFile('./dist/' + file, function (err, data) {
+					if (err) throw err
 
-			s3.putObject(params, function (err) {
-				if (err) console.log(err)
-				else console.log(file + ' uploaded')
+					let params = {
+						Bucket: branch2bucket[branch],
+						Key: 'fedor/' + file,
+						Body: data,
+						ContentType: mime.lookup(file)
+					}
+
+					s3.putObject(params, function (err) {
+						if (err) console.log(err)
+						else console.log(file + ' uploaded')
+					})
+				})
 			})
 		})
-	})
+	} else {
+		console.log('Nothing to do, only master and dev branches are deployed')
+	}
 })
