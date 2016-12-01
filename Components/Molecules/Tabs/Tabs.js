fbb86b81a68d5a17a18cf18cbb0d68e7f7e41fbb
Vlad Feninets <v.feninets@gmail.com>
2016-12-01 10:44:59 +0200

    Tabs improvements

--- Components/Molecules/Tabs/Tabs.js
+++ Components/Molecules/Tabs/Tabs.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Title from 'Utils/Title'
 
 
 const rubricsList = [
@@ -38,6 +37,37 @@ const rubricsList = [
 	'Юристы, адвокаты, нотариусы'
 ]
 
+
+const profsList = [
+	'Водитель',
+	'Бухгалтер',
+	'Продавец-консультант',
+	'Юрист',
+	'Администратор',
+	'Главный бухгалтер',
+	'Менеджер по продажам',
+	'Помощник руководителя',
+	'Экономист',
+	'Торговый представитель',
+	'Кладовщик',
+	'Официант',
+	'Офис-менеджер',
+	'Охранник',
+	'Секретарь',
+	'Уборщица',
+	'Кассир',
+	'Региональный менеджер',
+	'Грузчик',
+	'Курьер',
+	'Мерчендайзер',
+	'Продавец',
+	'Медицинский представитель',
+	'Оператор ПК',
+	'Менеджер по работе с клиентами',
+	'Директор',
+	'Системный администратор',
+]
+
 const CountLink = (props) => {
 	return (
 			<li>
@@ -50,32 +80,34 @@ const CountLink = (props) => {
 } 
 
 export default () => <div style={{ padding: '130px 200px' }} >
-	<Title>Outer Tabs</Title>
-
-
 	<div class="f-bg-ultra-blue" style={{ padding: '150px 50px' }}>
 
+
+
+
 		<div class="f-paper">
-
 			<div class="f-rubrics-wrap">
-
 				<div class="fd-merchant f-rubrics-outertabs-wrap">
 					<div class="fd-f-between-middle">
 						<div class="f-rubrics-outertab -activetab">
 							<i class="fi-category-find"></i>
 							Поиск по рубрикам
+							<a href="#"></a>
 						</div>
 						<div class="f-rubrics-outertab">
 							<i class="fi-profession-find"></i>
 							Поиск по профессиям
+							<a href="#"></a>
 						</div>
-						<div class="f-rubrics-outertab">
+						<div class="f-rubrics-outertab">	
 							<i class="fi-location-find"></i>
 							Поиск по городам
+							<a href="#"></a>
 						</div>
-						<div class="f-rubrics-outertab">
+						<div class="f-rubrics-outertab">							
 							<i class="fi-company-find"></i>
 							Поиск по компаниям
+							<a href="#"></a>
 						</div>
 					</div>
 				</div>
@@ -91,6 +123,45 @@ export default () => <div style={{ padding: '130px 200px' }} >
 						<ul class="f-reset-list">
 						{
 							rubricsList.map((rubric) => {
+								return <CountLink key={rubric} rubricName={rubric} />
+							})
+						}
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+		<br/>
+
+
+		<div class="f-paper">
+
+			<div class="f-rubrics-wrap">
+				<div class="f-rubrics-innerpaddings">
+
+					<div class="f-rubrics-innertabs-wrap">
+						<ul class="fd-soldier f-reset-list f-rubrics-innertabs-list">
+							<li class="f-rubrics-innertabs-item"><a href="#">По рубрикам</a></li>
+							<li class="fd-fat f-rubrics-innertabs-item -activetab"><a href="#">По профессиям</a></li>
+							<li class="f-rubrics-innertabs-item"><a href="">По городам</a></li>
+						</ul>
+					</div>
+
+					<div class="f-rubrics-itemsblock fd-merchant fd-list-3-cols">
+						<ul class="f-reset-list">
+						{
+							profsList.map((rubric) => {
 								return <CountLink key={rubric} rubricName={rubric} />
 							})
 						}
--- Pages/Tabs.js
+++ Pages/Tabs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import DemoWrapper from 'Components/DemoWrapper'
-import OuterTabs from 'Molecules/Tabs/OuterTabs'
+import Tabs from 'Molecules/Tabs/Tabs'
 import Header from 'Organisms/Header'
 import Footer from 'Organisms/Footer'
 
@@ -8,6 +8,6 @@ import Footer from 'Organisms/Footer'
 
 export default () => <div>
 	<Header title="Tabs" />
-	<OuterTabs />
+	<Tabs />
 	<Footer />
 </div>
