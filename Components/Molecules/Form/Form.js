7e2a8168fd825cf0e39b188d0b821ebcf2518912
Anastasiya Lozhachevskaya <stesha1990@mail.ru>
2016-12-05 18:27:41 +0200

    Form validation styles

--- Components/Molecules/Form/Form.js
+++ Components/Molecules/Form/Form.js
@@ -8,12 +8,28 @@ export default () => <div class="f-bg-gradient"  style={{ transition: '3s' }} >
 			<div class="f-paper fd-text-center">
 				<div class="f-form">
 					<h3 class="fd-thin-ronin ft-thin-syoi f-text-black">Войти в аккаунт</h3>
-					<div class="f-input f-input-block">
-						<input type="text" placeholder="Email" />
-					</div>
-					
-					<div class="f-input f-input-block">
-						<input type="password" placeholder="Password" />
+					<div class="f-form-inputs">
+						<div class="f-input f-input-block">
+							<input type="text" placeholder="Name" />
+						</div>
+
+						<div class="f-input f-input-block error">
+							<input type="text" placeholder="Email" />
+							<span>Неправильный формат ввода</span>
+						</div>
+						
+						<div class="f-input f-input-block">
+							<input type="password" placeholder="Password" />
+							<span>Пароль неверный</span>
+						</div>
+						<div class="f-input f-input-block">
+							<select name="select" id="1">
+								<option>Gender</option>
+								<option>Male</option>
+								<option>Female</option>
+							</select>
+							<i class="fi-drop-down-icon"></i>
+						</div>
 					</div>
 					<button class="f-btn-primary fd-btn-block fd-beefy-soldier">ВХОД</button>
 					<p class="f-text-divider"><span>или</span></p>
