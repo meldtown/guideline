288498e1646b0f6a3aa33c81ca8d873a12d6a4f5
Anastasiya Lozhachevskaya <stesha1990@mail.ru>
2016-12-07 18:32:58 +0200

    Sidebar styles

--- Components/SidebarMenu.js
+++ Components/SidebarMenu.js
@@ -2,22 +2,37 @@ import React from 'react'
 import { Link } from 'react-router'
 
 
-export default () => <div class="fd-merchant fd-p20" style={{ marginTop: '90px' }} >
-	<Link to="/" class="f-always-blue f-btn-control-royal-blue-link fd-soldier">
-		<span>Homepage</span>
-		<i class="fi-arrows-right-big" />
-	</Link>
+export default () => <div>
+	
 
-	<br/><br/>
-
-	<ul class="temp-sidebar-ul f-reset-list fd-p20" style={{ paddingLeft: '25px' }}>
-		<li><Link to="/buttons" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Buttons</Link></li>
-		<li><Link to="/colors" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Colors</Link></li>
-		<li><Link to="/typo" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Typo</Link></li>
-		<li><Link to="/controls" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Controls</Link></li>
-		<li><Link to="/grid" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Grid</Link></li>
-		<li><Link to="/papers" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Papers</Link></li>
-		<li><Link to="/tabs" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Tabs</Link></li>
-		<li><Link to="/form" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Form</Link></li>
+	<ul class="f-sidebar-menu fd-craftsmen f-reset-list">
+		<Link to="/" class="f-sidebar-button f-btn-control-coral-link fd-beefy-merchant">
+			<span>Homepage</span>
+			<i class="fi-arrows-right-big" />
+		</Link>
+		<li class="f-sidebar-menu-link">
+			<Link to="/buttons" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Buttons</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/colors" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Colors</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/typo" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Typo</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/controls" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Controls</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/grid" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Grid</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/papers" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Papers</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/tabs" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Tabs</Link>
+		</li>
+		<li class="f-sidebar-menu-link">
+			<Link to="/form" class="f-always-blue f-text-black" activeClassName="f-text-royal-blue">Form</Link>
+		</li>
 	</ul>
 </div>
